59165cbf0f9c12670836b2705fd815e20313b2db
Jack Horsburgh <jak.horsburgh@gmail.com>
2016-10-16 20:31:59 +0100

    Adds responsive UI, unfinished menu.

--- src/src/code/RSSFeedItem.java
+++ src/src/code/RSSFeedItem.java
@@ -1,70 +1,63 @@
-package code;
+package src.code;
 
-import java.util.ArrayList;
 
-//the overall RSS feed
-public class RSSFeed {
-	
+//the separate news items within the RSS feed
+public class RSSFeedItem {
+
 	String title;
 	String description;
 	String link;
-	String category;
+	String pubDate;
+	String media;
 	
-	//contains all the news stories from the feed
-	ArrayList<RSSFeedItem> items = new ArrayList<RSSFeedItem>();
-
-	public RSSFeed() {
+	public RSSFeedItem() {
 		
 	}
-
+	
 	//constructor fills the required RSS tags
-	public RSSFeed(String title, String description, String link) {
+	public RSSFeedItem(String title, String description, String link) {
 		this.title = title;
 		this.description = description;
 		this.link = link;
 	}
-
+	
 	public String getTitle() {
 		return title;
 	}
-
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-
+	
 	public String getDescription() {
 		return description;
 	}
-
-
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-
+	
 	public String getLink() {
 		return link;
 	}
-
-
+	
 	public void setLink(String link) {
 		this.link = link;
 	}
-
-	public String getCategory() {
-		return category;
+	
+	public String getPubDate() {
+		return pubDate;
 	}
 	
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public ArrayList<RSSFeedItem> getItems() {
-		return items;
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
 	}
 	
-	public void addItem(RSSFeedItem item) {
-		items.add(item);
+	public String getMedia() {
+		return media;
+	}
+	
+	public void setMedia(String media) {
+		this.media = media;
 	}
 }
